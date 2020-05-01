6a1a576f21f94a91556e6ac1ed7de8318a182aec
Rehan Mahmood <rehanone@gmail.com>
2020-05-01 00:24:30 -0400

    - Apply firewall rules if `ferm` is defined as firewall manager. It uses module https://forge.puppet.com/puppet/ferm for managing ferm.

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -2,21 +2,46 @@ class webmin::firewall () inherits webmin {
 
   assert_private("Use of private class ${name} by ${caller_module_name}")
 
-  if $webmin::firewall_manage and defined('::firewall') {
+  $port = $webmin::service_port
+
+  if $webmin::firewall_manage {
     if size($webmin::allowed_networks) > 0 {
       $webmin::allowed_networks.each |$network| {
-        firewall { "${webmin::service_port} Allow inbound Webmin connection on port: ${webmin::service_port} from: ${network}":
-          dport  => $webmin::service_port,
-          source => $network,
+        if defined('::firewall') {
+          firewall { "${port} Allow Webmin connection on port: ${port} from: ${network}":
+            dport  => $port,
+            source => $network,
+            proto  => tcp,
+            action => accept,
+          }
+        }
+
+        if defined('::ferm') {
+          ferm::rule { "${port} Allow Webmin connection on port: ${port} from: ${network}":
+            chain  => 'INPUT',
+            action => 'ACCEPT',
+            proto  => tcp,
+            dport  => $port,
+            saddr  => $network,
+          }
+        }
+      }
+    } else {
+      if defined('::firewall') {
+        firewall { "${port} Allow Webmin connection on port: ${port}":
+          dport  => $port,
           proto  => tcp,
           action => accept,
         }
       }
-    } else {
-      firewall { "${webmin::service_port} Allow inbound Webmin connection on port: ${webmin::service_port}":
-        dport  => $webmin::service_port,
-        proto  => tcp,
-        action => accept,
+
+      if defined('::ferm') {
+        ferm::rule { "${port} Allow Webmin connection on port: ${port}":
+          chain  => 'INPUT',
+          action => 'ACCEPT',
+          proto  => tcp,
+          dport  => $port,
+        }
       }
     }
   }
